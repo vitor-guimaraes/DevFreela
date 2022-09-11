34f75b9741b9584cde773b6aa2f5d4b063cfe658
Vitor Guimarães <guimvitor@gmail.com>
2022-09-11 11:40:23 -0300

    Aprofundando em Actions (implementando controller)

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,45 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post ([FromBody] CreateProjectModel createProject)
+        {
+            return CreatedAtAction(nameof(GetById), new { id = createProject.Id }, createProject);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectModel updateProject)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            GetById(id);
+
+            return NoContent();
+        }
+    }
+}
