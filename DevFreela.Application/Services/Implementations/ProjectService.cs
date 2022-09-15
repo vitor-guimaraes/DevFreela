c9ca9630058d5d4ff7996bd55d7433b910793436
Vitor Guimarães <guimvitor@gmail.com>
2022-09-14 21:26:52 -0300

    Implementação dos métodos do ProjectController e criação dos modelos de saída (viewmodels)

--- DevFreela.Application/Services/Implementations/ProjectService.cs
+++ DevFreela.Application/Services/Implementations/ProjectService.cs
@@ -1,5 +1,6 @@
 ﻿using DevFreela.Application.Services.Interfaces;
 using DevFreela.Application.ViewModels;
+using DevFreela.Core.Entities;
 using DevFreela.Infrastructure.Persistence;
 using System;
 using System.Collections.Generic;
@@ -18,42 +19,73 @@ namespace DevFreela.Application.Services.Implementations
         }
         public int Create(NewProjectInputModel inputModel)
         {
-            throw new NotImplementedException();
+            var project = new Project(inputModel.Title, inputModel.Description, inputModel.IdClient, inputModel.IdFreelancer, inputModel.TotalCost);
+            
+            _dbContext.Projects.Add(project);
+
+            return project.Id;
         }
 
         public void CreateComment(CreateCommentInputModel inputModel)
         {
-            throw new NotImplementedException();
+            var comment = new ProjectComment(inputModel.Content, inputModel.IdProject, inputModel.IdUser);
+
+            _dbContext.ProjectComments.Add(comment);
         }
 
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            var project = _dbContext.Projects.SingleOrDefault(p => p.Id == id);
+
+            project.Cancel();
         }
 
         public void Finish(int id)
         {
-            throw new NotImplementedException();
+            var project = _dbContext.Projects.SingleOrDefault(p => p.Id == id);
+
+            project.Finish();
         }
 
         public List<ProjectViewModel> GetAll(string query)
         {
-            throw new NotImplementedException();
+            var projects = _dbContext.Projects;
+
+            var projectsViewModel = projects
+                .Select(p => new ProjectViewModel(p.Title, p.CreatedAt))
+                .ToList();
+
+            return projectsViewModel;
         }
 
         public ProjectDetailsViewModel GetById(int id)
         {
-            throw new NotImplementedException();
+            var project = _dbContext.Projects.SingleOrDefault(p => p.Id == id);
+
+            var projectsDetailsViewModel = new ProjectDetailsViewModel(
+                project.Id,
+                project.Title,
+                project.Description,
+                project.TotalCost,
+                project.StartedAt,
+                project.FinishedAt
+                );
+
+            return projectsDetailsViewModel;
         }
 
         public void Start(int id)
         {
-            throw new NotImplementedException();
+            var project = _dbContext.Projects.SingleOrDefault(p => p.Id == id);
+
+            project.Start();
         }
 
         public void Update(UpdateProjectInputModel inputModel)
         {
-            throw new NotImplementedException();
+            var project = _dbContext.Projects.SingleOrDefault(p => p.Id == inputModel.Id);
+
+            project.Update(inputModel.Title, inputModel.Description, inputModel.TotalCost);
         }
     }
 }
--- DevFreela.Application/ViewModels/CreateCommentInputModel.cs
+++ DevFreela.Application/ViewModels/CreateCommentInputModel.cs
@@ -8,5 +8,8 @@ namespace DevFreela.Application.ViewModels
 {
     public class CreateCommentInputModel
     {
+        public string Content { get; set; }
+        public int IdProject { get; set; }
+        public int IdUser { get; set; }
     }
 }
