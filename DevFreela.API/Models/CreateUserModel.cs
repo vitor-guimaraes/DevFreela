7bd2523a68a51c941503dc8bd6c65dc08e00fdff
Vitor Guimarães <guimvitor@gmail.com>
2022-09-27 09:41:58 -0300

    git rm -r . --cached

--- DevFreela.API/Models/CreateUserModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DevFreela.API.Controllers
-{
-    public class CreateUserModel
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
