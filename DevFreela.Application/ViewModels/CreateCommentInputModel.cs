12872e35b7d1b435faabfdf2f8c7a6bd64113991
Vitor Guimarães <guimvitor@gmail.com>
2022-09-27 11:55:06 -0300

    Git Ignore

--- DevFreela.Application/ViewModels/CreateCommentInputModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DevFreela.Application.ViewModels
-{
-    public class CreateCommentInputModel
-    {
-        public string Content { get; set; }
-        public int IdProject { get; set; }
-        public int IdUser { get; set; }
-    }
-}
