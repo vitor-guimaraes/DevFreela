ad3f180f8cf60d38a2488af661a45ff275e812dc
Vitor-Guimaraes <guimvitor@gmail.com>
2023-06-20 16:27:42 -0300

    merge com branch de testes unitários

--- DevFreela.Infrastructure/Persistence/Repositories/UserRepository.cs
+++ DevFreela.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -59,7 +59,7 @@ namespace DevFreela.Infrastructure.Persistence.Repositories
 
         public async Task<User> GetUserByEmailAndPasswordAsync(string email, string passwordHash)
         {
-            return await _dbContext.Users.SingleOrDefaultAsync(u => u.Email == email && u.Password == passwordHash);
+            return await _dbContext.Users.SingleOrDefaultAsync(u => u.Email == email);// && u.Password == passwordHash);
         }
     }
 }
